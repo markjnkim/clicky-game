dfd367ab7d4ff612a65f706e8d810b0538e42521
trumpocalyspe <mark.jn.kim@gmail.com>
2018-08-16 18:20:29 -0700

    Initial Commit

---
+++ src/components/Game/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Container from '../Container/Container';
+import ClickItem from '../ClickItem/ClickItem';
+
+class Game extends Component {
+  
+}
+
+
+
+export default Game;
